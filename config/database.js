77f8bfd0faa10dd4fbf2f6ac29ca582f466806f0
Daniel Chung <chung.dan.s@gmail.com>
2015-11-22 15:21:53 -0500

    initial commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+    'url' : 'mongodb://daniel-chung-public:opamn6msav8qarscbpaek1q8vu@ds057254.mongolab.com:57254/test-auth'
+    //'mongodb://127.0.0.1/test'
+    // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+};
